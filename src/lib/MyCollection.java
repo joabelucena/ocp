739d20bee6a39910e7ab8287efe57aa232ed014e
Joabe Lucena <joabelucena@gmail.com>
2016-08-08 22:29:25 -0300

    ocp: lib

---
+++ src/lib/MyCollection.java
@@ -0,0 +1,87 @@
+package lib;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MyCollection<T> implements Iterable<T>, Collection<T> {
+	
+	private Collection<T> store;
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(T e) {
+		return this.store.add(e);
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/lib/TextElements.java
@@ -0,0 +1,15 @@
+package lib;
+
+public class TextElements {
+	public static final String SEPARATOR = "-----------------------------------------";
+	
+	public static final String MINISEPARATOR = "---------";
+	
+	public static void separe(){
+		System.out.println(SEPARATOR);
+	}
+	
+	public static void miniSepare(){
+		System.out.println(SEPARATOR);
+	}
+}
