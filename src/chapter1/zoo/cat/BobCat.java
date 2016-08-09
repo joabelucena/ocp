4ad294d6e821225b6028b443f7c4d612df6843b6
Joabe Lucena <joabelucena@gmail.com>
2016-08-08 22:26:16 -0300

    chapter1: @Override annotation

--- src/chapter1/zoo/cat/BobCat.java
+++ src/chapter1/zoo/cat/BobCat.java
@@ -3,3 +3,15 @@ package chapter1.zoo.cat;
 public class BobCat {
 	public void findDen(){}
 }
+
+class BobCatMother extends BobCat {
+	@Override
+	public void findDen() {}
+	
+	/*
+	 * DOES NOT COMPILE: @Override tells the compiler the following method is being overriding. Since there's not method
+	 * with the signature findDen(boolean c) within superclass, the code doesn't compile
+	 */
+	@Override
+	public void findDen(boolean c) {}
+}
