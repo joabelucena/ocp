9fe3764200bb2378630c9ecc9e9e2be480d36d5b
Joabe Lucena <joabelucena@gmail.com>
2016-08-11 22:45:57 -0300

    chapter3: Bounds: Upperbounded Wildcard

---
+++ src/chapter3/bounds/UpperBounded.java
@@ -0,0 +1,74 @@
+package chapter3.bounds;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Test classes
+ */
+class Sparrow extends Bird { }
+class Bird { }
+
+interface Flyer { void fly(); }
+class HangGlider implements Flyer { public void fly() { }  }
+class Goose implements Flyer { public void fly() { }  }
+
+/**
+ * 
+ * Bounded wildcards are specifications for receveing generic parameters. Its syntax is:
+ * 
+ * <? condition Type>
+ * 
+ * An easy way to remember is reading as:
+ * 
+ * ? 			- Any Class that
+ * condition	- condition
+ * Type			- of Type is acceptable
+ * 
+ * Upper-bounded wildcard eg: <? extends Object> 
+ * 
+ * @author root
+ *
+ */
+public class UpperBounded {
+	
+	
+
+	public static void main(String[] args) {
+		
+		{
+			List<? extends Bird> birds = new ArrayList<Bird>();
+			
+			/*
+			 * As the list becomes immutable you can't add new elements
+			 */
+//			birds.add(new Sparrow());
+//			birds.add(new Bird());
+			
+			List<? extends Exception> l = new ArrayList<RuntimeException>();
+		}
+		
+		{
+			List<Integer> list = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+			
+			System.out.println("The list's: " + list + " factorial is: " + factorial(list));	
+		}
+		
+
+	}
+	
+	public static long factorial(List<? extends Number> list) {
+		
+		long count = 1;
+				
+		for (Number number : list) {
+			count *= number.longValue();
+		}
+		
+		return count;
+	}
+	
+	private void anyFlyer(List<Flyer> flyer) { }
+	private void groupOfFLyers(List<? extends Flyer> flyer) { }
+}
